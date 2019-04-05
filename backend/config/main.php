bde6e56f947ce74fa27b9e8529fc02ce34598335
mou_1 <mou11262@gmail.com>
2019-04-06 00:13:19 +0700

    first commit

---
+++ backend/config/main.php
@@ -0,0 +1,70 @@
+<?php
+# Virtual Host
+use \yii\web\Request;
+$baseUrl = str_replace('/web', '', (new Request)->getBaseUrl());
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-backend',
+            # Virtual Host
+            'baseUrl' => $baseUrl,
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-backend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        
+        'urlManager' => [
+            'showScriptName' => false,   // Disable index.php
+            'enablePrettyUrl' => true,   // Disable r= routes
+            'enableStrictParsing' => true,
+            'rules' => [
+               '' => 'site/index',
+               'login'=>'site/login',
+               # Other
+                '<controller:\w+>' => '<controller>/index',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<id:\w+>' => '<controller>/<action>',
+                
+                # Modules
+                '<module:\w+>' => '<module>/default/index',
+                '<module:\w+>/<controller:\w+>' => '<module>/<controller>/index',
+                '<module:\w+>/<controller:\w+>/<action>' => '<module>/<controller>/<action>',      
+            ],
+        ],
+         
+    ],
+    'params' => $params,
+];
